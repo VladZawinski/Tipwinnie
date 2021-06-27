eefdb937fc4b91042f8c04de7ac2ca51ab7e63c8
ThihaKaungSet3 <Kaungset138@icloud.com>
2021-06-27 12:59:37 +0630

    remove smth

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 const app = require('express')();
+const mongoose = require('mongoose');
+
+require('dotenv').config()
+
+mongoose.connect(process.env.ATLAS_URL,{
+     useNewUrlParser: true,
+     useUnifiedTopology: true
+});
+
+const db = mongoose.connection
+
+db.on('error', e => console.log(e))
+db.once('open', function () {
+     console.log('Connected to database!');
+})
+
+
+
+
 
